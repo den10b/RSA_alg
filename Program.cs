2267247fa7e66245f2913fddb35af6d7e6af8221
Oleg <oleg.orlowsky@gmail.com>
2022-12-16 20:18:39 +0300

    Аdded generation of prime numbers

--- Program.cs
+++ Program.cs
@@ -17,7 +17,8 @@ namespace Elgamal_encryption
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            MessageBox.Show($"{Random()}, Test ");
+            MessageBox.Show($"{Random()}, Random_Digit ");
+            MessageBox.Show($"{GenerateNumbers()}, Generate_Primal_Number ");
         }
 
         unsafe static int Random()
@@ -26,6 +27,29 @@ namespace Elgamal_encryption
             int* ptr = &x;
             int lastNumber = (int)ptr % 10;
             return lastNumber < 0 ? -lastNumber : lastNumber;
+        }
+
+        static int[] GenerateNumbers()
+        {
+            int m = 6075,
+                a = 106,
+                c = 1283,
+                x = 6075;
+
+            int[] numbers = new int[10];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = x = (a * x + c) % m;
+            }
+
+            Array.Sort(numbers);
+
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            return numbers;
         }
     }
 }
